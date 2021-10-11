e3781e69351d0f919556c95e714c690e6793fb70
singhalyash8080 <you@example.com>
2021-10-11 17:38:04 +0530

    added code

---
+++ ascon.py
@@ -0,0 +1,246 @@
+#!/usr/bin/env python3
+
+"""
+Implementation of Ascon v1.2, an authenticated cipher
+"""
+
+# === Ascon AEAD encryption and decryption ===
+
+def ascon_encrypt(key, associateddata, nonce, plaintext): 
+    """
+    Ascon encryption.
+    key: a bytes object of size 16 (for Ascon-128; 128-bit security)
+    nonce: a bytes object of size 16 (must not repeat for the same key!)
+    plaintext: a bytes object of arbitrary length
+    returns a bytes object of length len(plaintext)+16 containing the ciphertext and tag
+    """
+    S = [0, 0, 0, 0, 0]
+    k = 128   # bits
+    a = 12   # rounds
+    b = 6   # rounds
+    rate = 8   # bytes
+
+    ascon_initialize(S, k, rate, a, b, key, nonce)
+    ascon_process_associated_data(S, b, rate, associateddata)
+    ciphertext = ascon_process_plaintext(S, b, rate, plaintext)
+    tag = ascon_finalize(S, rate, a, key)
+    return ciphertext + tag
+
+def ascon_process_associated_data(S, b, rate, associateddata):
+    """
+    Ascon associated data processing phase - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    b: number of intermediate rounds for permutation
+    rate: block size in bytes (8 for Ascon-128, 16 for Ascon-128a)
+    associateddata: a bytes object of arbitrary length
+    returns nothing, updates S
+    """
+    if len(associateddata) > 0:
+        a_zeros = rate - (len(associateddata) % rate) - 1
+        a_padding = to_bytes([0x80] + [0 for i in range(a_zeros)])
+        a_padded = associateddata + a_padding
+
+        for block in range(0, len(a_padded), rate):
+            S[0] ^= bytes_to_int(a_padded[block:block+8])
+            ascon_permutation(S, b)
+
+    S[4] ^= 1
+
+
+# === Ascon AEAD building blocks ===
+
+def ascon_initialize(S, k, rate, a, b, key, nonce):
+    """
+    Ascon initialization phase - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    k: key size in bits
+    rate: block size in bytes (8 for Ascon-128, Ascon-80pq; 16 for Ascon-128a)
+    a: number of initialization/finalization rounds for permutation
+    b: number of intermediate rounds for permutation
+    key: a bytes object of size 16 (for Ascon-128, Ascon-128a; 128-bit security) or 20 (for Ascon-80pq; 128-bit security)
+    nonce: a bytes object of size 16
+    returns nothing, updates S
+    """
+    iv_zero_key_nonce = to_bytes([k, rate * 8, a, b] + (20-len(key))*[0]) + key + nonce
+    S[0], S[1], S[2], S[3], S[4] = bytes_to_state(iv_zero_key_nonce)
+    
+ 
+    ascon_permutation(S, a)
+
+    zero_key = bytes_to_state(zero_bytes(40-len(key)) + key)
+    S[0] ^= zero_key[0]
+    S[1] ^= zero_key[1]
+    S[2] ^= zero_key[2]
+    S[3] ^= zero_key[3]
+    S[4] ^= zero_key[4]
+
+def ascon_process_plaintext(S, b, rate, plaintext):
+    """
+    Ascon plaintext processing phase (during encryption) - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    b: number of intermediate rounds for permutation
+    rate: block size in bytes (8 for Ascon-128)
+    plaintext: a bytes object of arbitrary length
+    returns the ciphertext (without tag), updates S
+    """
+    p_lastlen = len(plaintext) % rate
+    p_padding = to_bytes([0x80] + (rate-p_lastlen-1)*[0x00])
+    p_padded = plaintext + p_padding
+
+    # first t-1 blocks
+    ciphertext = to_bytes([])
+    for block in range(0, len(p_padded) - rate, rate):
+        S[0] ^= bytes_to_int(p_padded[block:block+8])
+        ciphertext += int_to_bytes(S[0], 8)
+
+        ascon_permutation(S, b)
+
+    # last block t
+    block = len(p_padded) - rate
+    S[0] ^= bytes_to_int(p_padded[block:block+8])
+    ciphertext += int_to_bytes(S[0], 8)[:p_lastlen]
+    
+    return ciphertext
+
+
+
+
+
+def ascon_finalize(S, rate, a, key):
+    """
+    Ascon finalization phase - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    rate: block size in bytes (8 for Ascon-128, Ascon-80pq; 16 for Ascon-128a)
+    a: number of initialization/finalization rounds for permutation
+    key: a bytes object of size 16 (for Ascon-128)
+    returns the tag, updates S
+    """
+    assert(len(key) in [16,20])
+    S[rate//8+0] ^= bytes_to_int(key[0:8])
+    S[rate//8+1] ^= bytes_to_int(key[8:16])
+    S[rate//8+2] ^= bytes_to_int(key[16:])
+
+    ascon_permutation(S, a)
+
+    S[3] ^= bytes_to_int(key[-16:-8])
+    S[4] ^= bytes_to_int(key[-8:])
+    tag = int_to_bytes(S[3], 8) + int_to_bytes(S[4], 8)
+    
+    return tag
+
+
+# === Ascon permutation ===
+
+def ascon_permutation(S, rounds=1):
+    """
+    Ascon core permutation for the sponge construction - internal helper function.
+    S: Ascon state, a list of 5 64-bit integers
+    rounds: number of rounds to perform
+    returns nothing, updates S
+    """
+    assert(rounds <= 12)
+    
+    for r in range(12-rounds, 12):
+        # --- add round constants ---
+        S[2] ^= (0xf0 - r*0x10 + r*0x1)
+        
+        # --- substitution layer ---
+        S[0] ^= S[4]
+        S[4] ^= S[3]
+        S[2] ^= S[1]
+        T = [(S[i] ^ 0xFFFFFFFFFFFFFFFF) & S[(i+1)%5] for i in range(5)]
+        for i in range(5):
+            S[i] ^= T[(i+1)%5]
+        S[1] ^= S[0]
+        S[0] ^= S[4]
+        S[3] ^= S[2]
+        S[2] ^= 0XFFFFFFFFFFFFFFFF
+        
+        # --- linear diffusion layer ---
+        S[0] ^= rotr(S[0], 19) ^ rotr(S[0], 28)
+        S[1] ^= rotr(S[1], 61) ^ rotr(S[1], 39)
+        S[2] ^= rotr(S[2],  1) ^ rotr(S[2],  6)
+        S[3] ^= rotr(S[3], 10) ^ rotr(S[3], 17)
+        S[4] ^= rotr(S[4],  7) ^ rotr(S[4], 41)
+        
+
+
+# === helper functions ===
+
+def get_random_bytes(num):
+    import os
+    return to_bytes(os.urandom(num))
+
+def zero_bytes(n):
+    return n * b"\x00"
+
+def to_bytes(l): # where l is a list or bytearray or bytes
+    return bytes(bytearray(l))
+
+def bytes_to_int(bytes):
+    return sum([bi << ((len(bytes) - 1 - i)*8) for i, bi in enumerate(to_bytes(bytes))])
+
+def bytes_to_state(bytes):
+    return [bytes_to_int(bytes[8*w:8*(w+1)]) for w in range(5)]
+
+def int_to_bytes(integer, nbytes):
+    return to_bytes([(integer >> ((nbytes - 1 - i) * 8)) % 256 for i in range(nbytes)])
+
+def rotr(val, r):
+    return (val >> r) | ((val & (1<<r)-1) << (64-r))
+
+def bytes_to_hex(b):
+    return b.hex()
+    #return "".join(x.encode('hex') for x in b)
+
+def printstate(S, description=""):
+    print(" " + description)
+    print(" ".join(["{s:016x}".format(s=s) for s in S]))
+
+def printwords(S, description=""):
+    print(" " + description)
+    print("\n".join(["  x{i}={s:016x}".format(**locals()) for i, s in enumerate(S)]))
+
+
+def demo_print(data):
+    maxlen = max([len(text) for (text, val) in data])
+    for text, val in data:
+        print("{text}:{align} 0x{val} ({length} bytes)".format(text=text, align=((maxlen - len(text)) * " "), val=bytes_to_hex(val), length=len(val)))
+
+def demo_aead():
+    keysize = 16
+    print("=== demo encryption using Ascon-128 ===")
+
+    # choose a cryptographically strong random key and a nonce that never repeats for the same key:
+    key   = get_random_bytes(keysize) # zero_bytes(keysize)
+    nonce = get_random_bytes(16)      # zero_bytes(16)
+
+    associateddata = b"ASCON"
+    
+    print()
+    print('Enter text to be encrypted : ',end = '')
+    plaintext      = input()
+
+    print()
+
+    plaintext = plaintext.encode('UTF-8')
+
+    ciphertext        = ascon_encrypt(key, associateddata, nonce, plaintext)
+        
+    demo_print([("key", key), 
+                ("nonce", nonce), 
+                ("ass. data", associateddata)
+               ])
+
+    print("plaintext: ", plaintext.decode('UTF-8'))
+
+    demo_print([
+                ("ciphertext", ciphertext[:-16]), 
+                ("tag", ciphertext[-16:]), 
+               ])
+
+    print()
+
+
+if __name__ == "__main__":
+    demo_aead()
